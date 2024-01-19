51885c421e634502e867b7082e31c5b2d8ef75a9
Lawrence Sebald <ljsebald@users.sourceforge.net>
2024-01-18 23:34:10 -0500

    Fix formatting a bit.

--- firmware/usb_descriptors.c
+++ firmware/usb_descriptors.c
@@ -76,7 +76,8 @@ uint8_t const *tud_descriptor_device_cb(void) {
 
 uint8_t const desc_hid_report[] = {
     TUD_HID_REPORT_DESC_GAMEPAD(HID_REPORT_ID(REPORT_ID_GAMEPAD)),
-    TUD_HID_REPORT_DESC_GENERIC_INOUT(CFG_TUD_HID_EP_BUFSIZE, HID_REPORT_ID(REPORT_ID_INOUT))
+    TUD_HID_REPORT_DESC_GENERIC_INOUT(CFG_TUD_HID_EP_BUFSIZE,
+                                      HID_REPORT_ID(REPORT_ID_INOUT))
 };
 
 // Invoked when received GET HID REPORT DESCRIPTOR
@@ -108,8 +109,7 @@ enum {
 #define EPNUM_HID_IN    0x83
 #define EPNUM_HID_OUT   0x03
 
-uint8_t const desc_configuration[] =
-{
+uint8_t const desc_configuration[] = {
   TUD_CONFIG_DESCRIPTOR(1, ITF_NUM_TOTAL, 0, CONFIG_TOTAL_LEN,
                         TUSB_DESC_CONFIG_ATT_REMOTE_WAKEUP, 500),
 
@@ -117,18 +117,16 @@ uint8_t const desc_configuration[] =
                      EPNUM_CDC_OUT, EPNUM_CDC_IN, CFG_TUD_CDC_TX_BUFSIZE),
 
   TUD_HID_INOUT_DESCRIPTOR(ITF_NUM_HID, 4, HID_ITF_PROTOCOL_NONE,
-                            sizeof(desc_hid_report), EPNUM_HID_OUT,
-                            EPNUM_HID_IN,
-                            CFG_TUD_HID_EP_BUFSIZE, 5)
+                           sizeof(desc_hid_report), EPNUM_HID_OUT,
+                           EPNUM_HID_IN, CFG_TUD_HID_EP_BUFSIZE, 5)
 };
 
 // Invoked when received GET CONFIGURATION DESCRIPTOR
 // Application return pointer to descriptor
 // Descriptor contents must exist long enough for transfer to complete
 uint8_t const *tud_descriptor_configuration_cb(uint8_t index) {
-    (void)index; // for multiple configurations
+    (void)index;
 
-    // This example use the same configuration for both high and full speed mode
     return desc_configuration;
 }
 
@@ -152,7 +150,8 @@ char const *string_desc_arr [] = {
 static uint16_t _desc_str[32];
 
 // Invoked when received GET STRING DESCRIPTOR request
-// Application return pointer to descriptor, whose contents must exist long enough for transfer to complete
+// Application return pointer to descriptor, whose contents must exist long
+// enough for transfer to complete
 uint16_t const *tud_descriptor_string_cb(uint8_t index, uint16_t langid) {
     uint8_t chr_count;
   
@@ -164,29 +163,31 @@ uint16_t const *tud_descriptor_string_cb(uint8_t index, uint16_t langid) {
     }
     else {
         // Note: the 0xEE index string is a Microsoft OS 1.0 Descriptors.
-        // https://docs.microsoft.com/en-us/windows-hardware/drivers/usbcon/microsoft-defined-usb-descriptors
-
+        // https://docs.microsoft.com/en-us/windows-hardware/drivers/usbcon/
+        // microsoft-defined-usb-descriptors
         if(index == 3)
             pico_get_unique_board_id_string(serial, sizeof(serial));
 
-        if(!(index < sizeof(string_desc_arr) / sizeof(string_desc_arr[0])))
+        if(!(index < sizeof(string_desc_arr) / sizeof(string_desc_arr[0]))) {
             return NULL;
+        }
+        else {
+            const char *str = string_desc_arr[index];
 
-        const char *str = string_desc_arr[index];
+            // Make sure not to overflow the static buffer
+            chr_count = strlen(str);
+            if(chr_count > 31)
+                chr_count = 31;
 
-        // Cap at max char
-        chr_count = strlen(str);
-        if(chr_count > 31)
-            chr_count = 31;
-
-        // Convert ASCII string into UTF-16
-        for(uint8_t i = 0; i < chr_count; i++) {
-            _desc_str[1+i] = str[i];
+            // Convert ASCII string into UTF-16
+            for(uint8_t i = 0; i < chr_count; i++) {
+                _desc_str[i + 1] = str[i];
+            }
         }
     }
 
     // first byte is length (including header), second byte is string type
-    _desc_str[0] = (TUSB_DESC_STRING << 8 ) | (2*chr_count + 2);
+    _desc_str[0] = (TUSB_DESC_STRING << 8) | (2 * chr_count + 2);
 
     return _desc_str;
 }
